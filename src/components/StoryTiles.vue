a0d4d7a091c4c0ffd188368507749f34ea90e896
Your Name <you@example.com>
2022-06-10 13:35:00 -1000

    Vue working with DRF backend

--- src/components/StoryTiles.vue
+++ src/components/StoryTiles.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="gameplay-container">
+    <div v-if="storytiles[0]" class="gameplay-container">
         <div class="storytiles_content">
-            <h1>Gameplay</h1>
+            <h1>{{ storytiles[prompt_id].story }} Gameplay</h1>
             
-            <p>{{ storytiles }}</p>
-
-            <button class="btn btn-primary">Next</button>
+            <p>{{ storytiles[prompt_id].prompt }} ({{storytiles[prompt_id].x_coord}}, {{storytiles[prompt_id].y_coord}})</p>
+            
+            <button @click='prompt_id++' class="btn btn-secondary mx-2 mb-2">Next Prompt (MVP)</button>
+            <br>
+            <button class="btn btn-secondary mx-2">Left</button>
+            <button class="btn btn-secondary mx-2">Right</button>
+            <br>
+            <button @click='prompt_id--' class="btn btn-secondary mx-2 mt-2">Backwards (MVP)</button>
+    
         </div>
     </div>
 </template>
@@ -15,7 +21,13 @@
         data() {
             return {
                 // StoryTiles
-                storytiles: []
+                storytiles: [],
+                prompt_id: 0,
+                x_coord: 0,
+                y_coord: 0,
+                story: '',
+                prompt: '',
+
             }
         },
         methods: {
@@ -27,15 +39,19 @@
                     // set the data returned as storytiles
                     this.storytiles = response.data;
                     console.log(this.storytiles)
+                    this.x_coord = parseInt(this.storytiles[0].x_coord)
+                    this.y_coord = parseInt(this.storytiles[0].y_coord)
                 } catch (error) {
                     // log the error
                     console.log(error)
                 }
             },
+            
         },
         created() {
             // Fetch storytiles on page load
-            this.getData();
+            this.getData()
+
         }
     }
 </script>
